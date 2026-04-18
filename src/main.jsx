be86226e69e920c27529ef6accabf1290c5f3ed7
Joshua Anthony Aurellano <jaaurellano@gbox.ncf.edu.ph>
2026-04-18 12:37:31 +0800

    Update vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
